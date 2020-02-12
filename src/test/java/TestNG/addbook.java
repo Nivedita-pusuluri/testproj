03f390e01762797395de2a2eb6b4201e7b2b2e4e
syam babu pusuluri <syambabu278@gmail.com>
2020-02-11 19:46:48 -0500

    Updaing URL

--- src/test/java/TestNG/addbook.java
+++ src/test/java/TestNG/addbook.java
@@ -10,28 +10,30 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class addbook {
-	  WebDriver driver;
+	WebDriver driver;
+
 	@BeforeMethod
 	public void launch() {
-		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-		  driver = new ChromeDriver();
-		  driver.get("http://52.66.244.143:32768/addressbook/");
-		  driver.manage().window().maximize();
-		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://18.204.219.32:32770/addressbook/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 	}
-  @Test
-  public void verifyaddbook() {
-	  driver.findElement(By.xpath("//div[@class='v-button v-widget']")).click();
-	  driver.findElement(By.id("gwt-uid-5")).sendKeys("Edureka");
-	  driver.findElement(By.id("gwt-uid-7")).sendKeys("Selenium");
-	  driver.findElement(By.id("gwt-uid-9")).sendKeys("9876543210");
-	  driver.findElement(By.id("gwt-uid-11")).sendKeys("devops@gmail.com");
-	  driver.findElement(By.id("gwt-uid-13")).sendKeys("06/11/1986");
-	  driver.findElement(By.xpath("//div[@class='v-button v-widget primary v-button-primary']")).click();
-  }
-  
-  @AfterMethod
-  public void close() {
-	  driver.close();
+
+	@Test
+	public void verifyaddbook() {
+		driver.findElement(By.xpath("//div[@class='v-button v-widget']")).click();
+		driver.findElement(By.id("gwt-uid-5")).sendKeys("Edureka");
+		driver.findElement(By.id("gwt-uid-7")).sendKeys("Selenium");
+		driver.findElement(By.id("gwt-uid-9")).sendKeys("9876543210");
+		driver.findElement(By.id("gwt-uid-11")).sendKeys("devops@gmail.com");
+		driver.findElement(By.id("gwt-uid-13")).sendKeys("06/11/1986");
+		driver.findElement(By.xpath("//div[@class='v-button v-widget primary v-button-primary']")).click();
+	}
+
+	@AfterMethod
+	public void close() {
+		driver.close();
 	}
 }
